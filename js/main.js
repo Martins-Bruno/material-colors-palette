796e22c63dda2bb45fe03683833dea7b62f6a31e
Martins-Bruno <46404375+Martins-Bruno@users.noreply.github.com>
2019-05-02 22:34:59 -0300

    Minor changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-// Constante que recebe a array com cada div dos .color-box;
+// Constante que recebe a NodeList com cada div dos .color-box:
 const colors = document.querySelectorAll('.color-box div');
 
-
-// Função que aplica as cores e o atributo do evento no HTML;
-// Serve para reduzir a quantidade de código no HTML;
+// Função que aplica as cores e o atributo do evento click no HTML (serve para reduzir a quantidade de código no HTML):
 function applyColor() {
 
   colors.forEach(function(element) {
@@ -13,14 +11,13 @@ function applyColor() {
     element.setAttribute('onclick', 'copyTextColor(this)');
 
   })
-  
 }
 
-// Chamada da função que aplica as cores e o atributo;
+// Chamada da função que aplica as cores e o atributo:
 applyColor();
 
 
-// Função que seleciona a cor do elemento div e o copia para o clipboard; 
+// Função que seleciona a cor do elemento div clicado e o copia para o clipboard: 
 function copyTextColor(e) {
 
   let selectedColor = e.innerHTML;
@@ -29,7 +26,12 @@ function copyTextColor(e) {
   clipboardText.select();
   document.execCommand('copy');
 
+  // Cria a mensagem de cópia:
   let mensagem = document.querySelector('.message').innerHTML = "Cor " + selectedColor + " copiada";
+
+  // Aplica a cor clicada no fundo da mensagem:
+  let corMensagem = document.querySelector('.message');
+  corMensagem.style.backgroundColor = selectedColor;
 
   let box = document.querySelector('.message-field');
   box.style.visibility = "visible";
@@ -46,7 +48,6 @@ function copyTextColor(e) {
     box.style.visibility = "hidden";
     box.style.opacity = "0";
   }, 900);
-
 
 }
 
